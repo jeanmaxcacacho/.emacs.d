faba0c362fcda96e6372d970660d47f123c9ab02
Jean Maximus C. Cacacho <maxcacacho@david.binondo>
2025-12-23 15:56:41 +0800

    init commit

---
+++ init.el
@@ -0,0 +1,20 @@
+;; -*- lexical-binding: 
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+(add-to-list 'default-frame-alist
+             '(font . "VictorMono Nerd Font Mono-12"))
+
+(load-theme 'modus-vivendi t)
+(add-to-list 'default-frame-alist '(alpha . 80))
+
+(require 'package)
+
+(setq package-archives
+      '(("gnu"   . "https://elpa.gnu.org/packages/")
+	("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
